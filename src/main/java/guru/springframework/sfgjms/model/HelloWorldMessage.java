90d7960d11fab805503d0d9f0f28936441e51bb3
Sebastian Szwaczyk <sszwaczyk@gmail.com>
2019-08-20 13:45:38 +0200

    Sending Hello message

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private static final long serialVersionUID = 5536812332265103753L;
+
+    private UUID id;
+    private String message;
+
+}
